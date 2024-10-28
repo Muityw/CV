5657d6df1b729746f8aae7d01fe76085f1a31e40
Muityw <nyckgame@gmail.com>
2024-10-27 23:18:15 -0300

    abluebu

---
+++ script.js
@@ -0,0 +1,16 @@
+// JavaScript para rolagem suave
+document.querySelectorAll('#floating-menu a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
